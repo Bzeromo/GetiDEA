8ee9decc5f5a0074c26b43374ddeccfe42590e5b
박영규 <dudrb5260@naver.com>
2024-01-22 15:59:39 +0900

    add: 추가 project 빌드

---
+++ back/giBack/src/main/java/com/gi/giback/GiBackApplication.java
@@ -0,0 +1,13 @@
+package com.gi.giback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiBackApplication.class, args);
+    }
+
+}
