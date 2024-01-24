47b6bbe684437ae6db6a9c3d34870852955fa39e
lhs <lhs2689@naver.com>
2024-01-24 17:30:18 +0900

    Feat: OAuthController 추가

--- back/giBack/src/main/java/com/gi/giback/mysql/controller/OAuth2Controller.java
+++ back/giBack/src/main/java/com/gi/giback/mysql/controller/OAuth2Controller.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/oauth2")
+@RequestMapping("/oauth")
 public class OAuth2Controller {
 
 }
