32cbf35eebaf2c814ecb009213f4c8ccba4bd24e
JIEUN <lju0514@naver.com>
2024-01-29 17:50:33 +0900

    Feat: 음성채팅 연습용 webRTC_test
    Change-Id: I98f43466fd55b8bf6916239c13b0383ec0b7e665

---
+++ webRTC_test/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter,Route, Routes } from "react-router-dom";
+import VideoCall from "./Component/VideoCall";
+// import Home from "./Component/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+       <Routes>
+        <Route path="/"  element={<VideoCall />} />
+      </Routes>
+    </BrowserRouter>
+   
+  );
+}
+
+export default App;
