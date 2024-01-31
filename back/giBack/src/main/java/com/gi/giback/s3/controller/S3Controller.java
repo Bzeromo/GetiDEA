2e34dc8fa28f7be76e8a63ab075bd6ec45a56d20
김범수 <kkkbs41@daum.net>
2024-01-31 15:41:12 +0900

    Feat: AWS S3 이미지 업로드 controller, servie 생성
    Change-Id: Ic710c335f8b2dd05d1ac650087edaa603e9e8458

---
+++ back/giBack/src/main/java/com/gi/giback/s3/controller/S3Controller.java
@@ -0,0 +1,30 @@
+package com.gi.giback.s3.controller;
+
+import com.gi.giback.s3.service.S3UploadService;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/image")
+@RequiredArgsConstructor
+public class S3Controller {
+
+    private final S3UploadService s3UploadService;
+
+    @PostMapping("/thumbnailImage")
+    public ResponseEntity<String> uploadThumbnailImage(@RequestParam("thumbnailImage") MultipartFile multipartFile) {
+        String result;
+        try {
+            result = s3UploadService.saveFile(multipartFile);
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+}
