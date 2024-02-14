5fe0cc93b8902df8b10464199adb15cce29898f1
정연우 <yoanwoo@naver.com>
2024-02-14 16:28:40 +0900

    Feat: KakaoLogin 페이지 수정
    Change-Id: I18ce735e7d6140fe4b3503344dab7e16ffcf593b

--- GetiDEA_front/src/pages/KakaoLogin.tsx
+++ GetiDEA_front/src/pages/KakaoLogin.tsx
@@ -1,23 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { useNavigate,useSearchParams } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
+import Cookies from 'js-cookie';
 
 const KakaoLogin: React.FC = () => {
   const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
 
-  const accessToken = searchParams.get("access_token");
-  const refreshToken = searchParams.get("refresh_token");
+  const [accessToken, setAccessToken] = useState<string | undefined>();
+  const [refreshToken, setRefreshToken] = useState<string | undefined>();
 
   useEffect(() => {
-    localStorage.setItem('accessToken',accessToken ?? "" )
-    localStorage.setItem('refreshToken',refreshToken ?? "" )
+    // 쿠키에서 토큰 읽어오기
+    const access_Token = Cookies.get('access_token');
+    const refresh_Token = Cookies.get('refresh_token');
 
+    console.log(Cookies.get('access_token'));
+    console.log(refreshToken);
+    // 상태 업데이트
+    setAccessToken(access_Token);
+    setRefreshToken(refresh_Token);
 
-    if(accessToken){
-      const decoded=jwtDecode(localStorage.getItem('accessToken')??"");
-      console.log(`토큰 디코딩 결과 : ${JSON.stringify(decoded)}`);
-      navigate("/home");
+    // 로컬 스토리지에 저장
+    if (accessToken && refreshToken) {
+      // JWT 디코딩
+      try {
+        const decoded = jwtDecode(accessToken);
+        console.log(`토큰 디코딩 결과: ${JSON.stringify(decoded)}`);
+        navigate("/home");
+      } catch (error) {
+        console.error("토큰 디코딩 실패:", error);
+      }
     }
   
   }, []);
